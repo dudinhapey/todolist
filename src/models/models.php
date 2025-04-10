2828bb3ae3dbab970a56e7909cb4d75af459c29e
Eduarda <eduarda.soligo@duque.g12.br>
2025-04-10 20:58:27 -0300

    CRiando as funções de adição e conclusão das tarefas.

--- src/models/models.php
+++ src/models/models.php
@@ -1,16 +1,33 @@
 <?php
     // Retorna as tarefas da sessão ou inicia um array vazio
-    function getTaskx(){
+    function getTasks(){
         // Digite o seu código aqui
+        return $_SESSION['tasks'] ?? [];
     }
 
     // Adiciona uma nova task no array
-    function addNewTask($description){
+    function addTask($description){
         // Seu código aqui
+        $tasks = getTasks();
+        $tasks[] = [
+            'id' => count($taks) + 1,
+            'description' => $description,
+            'completed' => false
+        ];
+
+        $_SESSION ['tasks'] = $tasks;
     }
 
     // Marca uma task como concluida
     function ToggleTask($id){
         // Seu código aqui
+        $tasks = getTasks();
+        foreach($tasks as $index => $tasks){
+            if($task['id'] == $id){
+                $tasks[$index]['completed'] = !$task['completed'];
+                break;
+            }
+        }
+        $_SESSION['tasks'] = $tasks;
     }
 ?>
